c151a75145c8243b4f45659de840ad93cd2a91e3
Robert Zavala <rbrtzavala@gmail.com>
2024-07-03 18:36:56 -0600

    nested hashes exercies

--- ruby_basics/12_nested_collections/exercises/nested_hash_exercises.rb
+++ ruby_basics/12_nested_collections/exercises/nested_hash_exercises.rb
@@ -1,6 +1,12 @@
+nested_lang ={
+  ruby: { initial_release: 'December 25, 1996', is_beautiful?: true },
+  javascript: { initial_release: 'December 4, 1995', is_beautiful?: false }
+}
+
 def find_language_information(languages, language_name)
   # Take languages (a nested hash) and language_name as a symbol, return the
   # value for the language_name key (which will be another hash!)
+  languages[language_name]
 
   # the languages hash will look something like this:
   # {
@@ -11,27 +17,36 @@ end
 
 def add_information_about_language(languages, language_name, info_key, info_value)
   # Take languages and add the key/value pair info_key/info_value to the nested
+  languages[language_name][info_key] = info_value
   # hash of language_name, then return the updated languages hash
+  languages
 end
 
 def add_language(languages, language_name, language_info_value)
   # Take languages and add the key/value pair language_name/language_info_value
+  languages[language_name] = language_info_value
   # to it, then return languages
+  languages
 end
 
 def delete_information_about_language(languages, language_name, info_key)
   # Take languages and delete the key/value pair with key info_key from
+  languages[language_name].delete(info_key)
   # language_name, then return languages
+  languages
 end
 
 def delete_language(languages, language_name)
   # Take languages and delete the language_name key/value pair, then return
+  languages.delete(language_name)
   # languages
+  languages
 end
 
 def find_beautiful_languages(languages)
   # Take languages and return a hash containing only languages which have the
   # key/value pair { is_beautiful?: true } listed in their information
+  languages.select { |lang, data| data[:is_beautiful?] == true }
 end
 
 def find_language_facts(languages, language_name, fact_index = 0)
@@ -39,6 +54,9 @@ def find_language_facts(languages, language_name, fact_index = 0)
   # key :facts and value of an array of strings) and return the fact
   # language_name has at fact_index of its facts array, or at index 0 if this
   # argument is not given
+  if languages[language_name]
+    languages[language_name][:facts][fact_index]
+  end
 
   # the revised languages hash will look something like this:
   # {
